628ffaf414381919b34a259e6ebb66b1f99d31e9
Edward <e.arekhva@gmail.com>
2020-09-06 12:41:22 +0200

    Revert "Revert "bugfix""
    This reverts commit f20452789a251d49096978bf11eee932b48445f5.

--- js/components/search.js
+++ js/components/search.js
@@ -1,18 +1,13 @@
-import DBService, {renderCard} from '../services/utils';
-
-export default search = () => {
-  const searchForm = document.querySelector('.search__form');
-  const searchInput = document.querySelector('.search__form-input');
-
-  const onSearchFormSubmit = function(evt) {
-    evt.preventDefault()
-    const searchValue = searchInput.value.trim();
-    if(searchValue) {
-      searchInput.value = '';
-      tvShows.append(loading);
-      new DBService().getSearchResult(searchValue).then(renderCard);
-    }
-  };
-
-  searchForm.addEventListener('submit', onSearchFormSubmit);
+const searchForm = document.querySelector(".search__form");
+const searchInput = document.querySelector(".search__form-input");
+const onSearchFormSubmit = function (evt) {
+  evt.preventDefault();
+  const searchValue = searchInput.value.trim();
+  if (searchValue) {
+    searchInput.value = "";
+    tvShows.append(loading);
+    new DBService().getSearchResult(searchValue).then(renderCard);
+  }
 };
+
+searchForm.addEventListener("submit", onSearchFormSubmit);
